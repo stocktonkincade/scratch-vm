a0b8897ff0b12c09239eeafe8df018887d3f8d5c
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2017-07-21 17:14:29 -0400

    WIP on physics

--- src/blocks/scratch3_physics.js
+++ src/blocks/scratch3_physics.js
@@ -1,7 +1,7 @@
 const Cast = require('../util/cast');
 const log = require('../util/log');
 const Clone = require('../util/clone');
-const Physics = require('matter-js');
+const Matter = require('matter-js');
 
 class Scratch3PhysicsBlocks {
 
@@ -15,33 +15,34 @@ class Scratch3PhysicsBlocks {
         this.runtime.HACK_PhysicsBlocks = this;
 
         // module aliases
-        var Engine = Matter.Engine,
-            Render = Matter.Render,
-            World = Matter.World,
-            Bodies = Matter.Bodies;
+        this.Engine = Matter.Engine;
+        // this.Render = Matter.Render;
+        this.World = Matter.World;
+        this.Bodies = Matter.Bodies;
 
         // create an engine
-        var engine = Engine.create();
+        this.engine = this.Engine.create();
 
         // create a renderer
-        var render = Render.create({
-            element: document.body,
-            engine: engine
-        });
+        // var render = this.Render.create({
+        //     element: document.body,
+        //     engine: this.engine
+        // });
+        //
+        this.engine.world.gravity.y = -1;
 
-        // create two boxes and a ground
-        var boxA = Bodies.rectangle(400, 200, 80, 80);
-        var boxB = Bodies.rectangle(450, 50, 80, 80);
-        var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+        this.ground = this.Bodies.rectangle(-240, -130, 480, 100, { isStatic: true });
+
+        this.bodies = [];
 
         // add all of the bodies to the world
-        World.add(engine.world, [boxA, boxB, ground]);
+        this.World.add(this.engine.world, this.ground);
 
         // run the engine
-        Engine.run(engine);
+        // Engine.run(engine);
 
         // run the renderer
-        Render.run(render);
+        // this.Render.run(this.render);
     }
 
     /**
@@ -58,6 +59,7 @@ class Scratch3PhysicsBlocks {
      */
     static get DEFAULT_PHYSICS_STATE () {
         return {
+            body: null
         };
     }
 
@@ -75,6 +77,53 @@ class Scratch3PhysicsBlocks {
         return physicsState;
     }
 
+    start () {
+        window.requestAnimationFrame(this.step.bind(this));
+    }
+
+    step () {
+        // todo:
+        // for each target
+        // if it has no body, create one, and add it using custom state
+        // set position of body to position of sprite (coordinate systems!)
+        for (let i=1; i<this.runtime.targets.length; i++) {
+            const target = this.runtime.targets[i];
+            const state = this._getPhysicsState(target);
+            if (!state.body) {
+                const options = {
+                    restitution: 0.8
+                };
+                console.log(target);
+                const body = this.Bodies.rectangle(0, 0, 50, 50, options );
+                this.bodies.push(body);
+                state.body = body;
+            }
+        }
+
+        /*
+
+        // check the position of each target and update it in the engine
+        // in case it has been moved by a motion block or a drag
+        for (let i=1; i<this.runtime.targets.length; i++) {
+            const target = this.runtime.targets[i];
+            const updatedPos = {x:target.x, y:target.y};
+            // todo: check if the position has actually changed before doing this
+            // and if so, zero out the velocities
+            Matter.Body.setPosition(this.boxes[i], updatedPos);
+        }
+        // update the physics engine
+        this.Engine.update(this.engine, 1000/60);
+        // update the positions of the targets
+        for (let i=1; i<this.runtime.targets.length; i++) {
+            const target = this.runtime.targets[i];
+            target.setXY(this.boxes[i].position.x, this.boxes[i].position.y);
+        }
+
+        */
+
+        window.requestAnimationFrame(this.step.bind(this));
+    }
+
     /**
      * Retrieve the block primitives implemented by this package.
      * @return {object.<string, Function>} Mapping of opcode to Function.
@@ -86,15 +135,13 @@ class Scratch3PhysicsBlocks {
     }
 
     getHats () {
-        return {
+        // return {
             // speech_whenihear: {
             //     restartExistingThreads: false,
             //     edgeActivated: true
-            }
-        };
+            // }
+        // };
     }
-
-
 }
 
 module.exports = Scratch3PhysicsBlocks;
